e24acd4f7c18b226f0c85c44eadb1b9e48bcf9e7
Aleticod <193837@unsaac.edu.pe>
2023-09-28 23:14:57 -0500

    :sparkles: Add example app: Cookie session app example

---
+++ examples/cookie-sessions/index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+var cookieSession = require('cookie-session')
+var express = require('express')
+
+var app = express()
+
+// add req.session cookie support
+app.use(cookieSession({
+  secret: 'manny is cool'
+}))
+
+// do something with the session
+app.use(count)
+
+// custom middleware
+function count(req, res) {
+  req.session.count = (req.session.count || 0) + 1
+  res.send('viewed ' + req.session.count + ' times\n')
+}
+
+// instanbul ignore next
+
+if (!module.parent) {
+  app.listen(3000)
+  console.log('Express started on port 3000')
+}
