c3916f889292af5f5b3423fda436580038f4ce79
Aleticod <193837@unsaac.edu.pe>
2023-10-01 19:32:13 -0500

    :sparkles: Add routing example: App chainable route with app.route()

--- routing/index.js
+++ routing/index.js
@@ -30,7 +30,7 @@ app.get('/user/:userId(\\d+)', (req, res) => {
 })
 
 app.listen(3000, () => {
-  console.log('random.txt')
+  console.log('The server is listening on PORT 3000')
 })
 
 // handlers
@@ -66,3 +66,16 @@ app.get('/example/d', [cb0, cb1], (req, res, next) => {
 }, (req, res) => {
   res.send('Hello from D')
 })
+
+// app.route()
+
+app.route('/book')
+  .get((req, res) => {
+    res.send('Get a random book')
+  })
+  .post((req, res) => {
+    res.send('Add a book')
+  })
+  .put((req, res) => {
+    res.send('Update the book')
+  })
